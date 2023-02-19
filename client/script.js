d9a009342919ee2160ceec021cfc2d1cdc878af1
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2023-02-20 01:18:23 +0530

    connecting client and server

--- client/script.js
+++ client/script.js
@@ -67,6 +67,31 @@ const handleSubmit = async (e) => {
 
   const messageDiv = document.getElementById(uniqueId);
   loader(messageDiv);
+
+  //fetch data from server --> bot's response
+  const response = await fetch("http://localhost:5000", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt: data.get("prompt"),
+    }),
+  });
+
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = "";
+
+  if (response.ok) {
+    const data = await response.json();
+    const parsedData = data.bot.trim();
+
+    typeText(messageDiv, parsedData);
+  } else {
+    const error = await response.text();
+    messageDiv.innerHTML = "Something went wrong...";
+    alert(error);
+  }
 };
 
 form.addEventListener("submit", handleSubmit);
